9563a77f88d9b8a1924064cfbfc5ba2bbf671b33
yuyilei <yyl1352091742@gmail.com>
2020-02-18 23:40:46 +0800

    feature: show user's coin and daily task

--- pages/mycoin/mycoin.js
+++ pages/mycoin/mycoin.js
@@ -1,18 +1,48 @@
 // pages/mycoin/mycoin.js
+const app = getApp()
+const requestHandler = require('../../common/requestHandler.js')
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    myCoinCount: 0,
+    taskCount: { 
+      login: 0, 
+      collect: 0, 
+      publish: 0, 
+      comment: 0, 
+     }, 
+     maxTaskCount: {
+      login: 1, 
+      collect: 5, 
+      publish: 5, 
+      comment: 5, 
+     }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.showToast({
+      title: '做任务得书币！',
+      duration: 2000,
+    })
+    var that = this
+    var url = "/user/coin/"
+    var data = {
+    }
+    var header = { token: app.globalData.token }
+    requestHandler.syncRequest(url, data, header, "GET").then(res => {
+      var data = res.data 
+      that.setData({
+        myCoinCount: data['coin_count'], 
+        taskCount: data['tasks']
+      })
+    }) 
   },
 
   /**
